b82b79df967eb14315efddb7905359823ad094d1
Marc Schmidt <mrc.schmidt@gmx.net>
2022-12-25 03:30:16 +0100

    - create empty home-page

--- frontend/src/providers/router.tsx
+++ frontend/src/providers/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../pages/app';
 import ErrorFallback from '../pages/error-fallback/error-fallback';
 
+const Home = lazy(() => import('../pages/home/home'));
 const PopularShows = lazy(() => import('../pages/popular-shows/popular-shows'));
 const UpcomingReleases = lazy(() => import('../pages/upcoming-releases/upcoming-releases'));
 
@@ -12,6 +13,10 @@ export const Router = createBrowserRouter([
         element: <App/>,
         errorElement: <ErrorFallback/>,
         children: [
+            {
+                path: '/',
+                element: <Home/>,
+            },
             {
                 path: '/shows/popular',
                 element: <PopularShows/>,
